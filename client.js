557c18cba84a24094c0a2c43b9258c0864e21aa3
Michael Truong <truong.michael.2013@gmail.com>
2025-05-15 19:20:06 -0400

    Add polling and streaming functionality to the client.js file

--- client.js
+++ client.js
@@ -29,18 +29,50 @@ function displayMessage(message) {
   console.log(`> ${message.username}: ${message.text}`);
   // sets value to boolean true
   displayedMessages[message.id] = true;
-}
+};
 
+// in JS, if we're doing a network request then we need to add async/await
 async function getAndDisplayMessages() {
   // gets all the messages inside the messagingAPI
   const messages = await messagingAPI.getMessages();
 
   for (const message of messages) {
-    // gets the boolean value 
+    // gets the boolean value
+    // this is checking if the message.id is inside the displayedMessages Object
+    // and the return value is true or false
     const messageAlreadyDisplayed = message.id in displayedMessages;
 
     // if it's not displayed, display it
     // practice single-line if statements
     if (!messageAlreadyDisplayed) displayMessage(message);
-  }
+  };
+}
+
+function pollMessages() {
+  // hey, every 3000 milliseconds, or 3 seconds, do this and get the messages
+  setInterval(getAndDisplayMessages, 3000);
+};
+
+function streamMessages() {
+  // this is the socket connection
+  const messagingSocket = messagingAPI.createMessagingSocket();
+
+  // this is the event listener for the socket connection
+  messagingSocket.on('message', message => {
+    // this is the JSON.parse method, which converts the message into an object
+    const parsedMessage = JSON.parse(message);
+
+    const messageAlreadyDisplayed = parsedMessage.id in displayedMessages;
+
+    // this is the displayMessage function
+    if (!messageAlreadyDisplayed) displayMessage(parsedMessage);
+  });
+};
+
+if (process.env.MODE === 'poll') {
+  getAndDisplayMessages();
+  pollMessages();
+} else if (process.env.MODE === 'stream') {
+  getAndDisplayMessages();
+  streamMessages();
 }
