69d9ecd5862d790a5e7d23daaab5c3d2fbdad961
Michael Truong <truong.michael.2013@gmail.com>
2025-05-15 18:40:13 -0400

    Add more functionality to server.js

--- server.js
+++ server.js
@@ -13,3 +13,26 @@ const messages = [
 ];
 
 const sockets = [];
+
+// app
+app.use(express.json());
+
+// invoke .listen() on express server app
+// this opens the port on 3001
+// takes callback
+app.listen(3001, () => { 
+  console.log('Listeing on port 3001!');
+});
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
+})
+
+app.post('/messages', (req, res) => {
+  const message = req.body;
+  messages.push(message);
+  
+  for (const socket of sockets) {
+    socket.send(JSON.stringify(message));
+  }
+});
